ab6d91614e7fc4c6200636e73b17df95e6f60450
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2024-08-07 07:36:00 +0800

    bugfix

--- prettier.config.js
+++ prettier.config.js
@@ -1,37 +1,32 @@
 /** @type {import('prettier').Config & import('@ianvs/prettier-plugin-sort-imports').PluginConfig & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
-  bracketSameLine: false,
-  endOfLine: "lf",
-  importOrder: [
-    "^(next/(.*)$)|^(next$)|^(react/(.*)$)|^(react$)", // Next.js and React.js modules
-    "<BUILTIN_MODULES>", // Node.js built-in modules
-    "<THIRD_PARTY_MODULES>", // NPM packages
-    "",
-    "^@/client/(.*)$",
-    "",
-    "^@/server/(.*)$",
-    "",
-    "^@/shared/(.*)$",
-    "",
-    "^@/trpc/(.*)$",
-    "",
-    "^@/(.*)$",
-    "",
-    "^(?!.*[.]css$)[./].*$", // Relative imports
-    ".css$", // CSS imports
-  ],
-  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
-  plugins: [
-    "@ianvs/prettier-plugin-sort-imports",
-    "prettier-plugin-tailwindcss",
-  ],
-  printWidth: 120,
-  quoteProps: "consistent",
-  semi: false,
-  singleQuote: true,
-  tabWidth: 2,
-  trailingComma: "none",
-  useTabs: true,
-};
+	bracketSameLine: false,
+	endOfLine: 'lf',
+	importOrder: [
+		'^(next/(.*)$)|^(next$)|^(react/(.*)$)|^(react$)', // Next.js and React.js modules
+		'<BUILTIN_MODULES>', // Node.js built-in modules
+		'<THIRD_PARTY_MODULES>', // NPM packages
+		'',
+		'^@/client/(.*)$',
+		'',
+		'^@/server/(.*)$',
+		'',
+		'^@/shared/(.*)$',
+		'',
+		'^@/(.*)$',
+		'',
+		'^(?!.*[.]css$)[./].*$', // Relative imports
+		'.css$' // CSS imports
+	],
+	importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
+	plugins: ['@ianvs/prettier-plugin-sort-imports', 'prettier-plugin-tailwindcss'],
+	printWidth: 120,
+	quoteProps: 'consistent',
+	semi: false,
+	singleQuote: true,
+	tabWidth: 2,
+	trailingComma: 'none',
+	useTabs: true
+}
 
-export default config;
+export default config
