9edc7826f1e893928c1a28d0e2f332f81542d83d
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2024-08-07 06:50:34 +0800

    fix: formatting

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,14 +1,11 @@
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import {
-  getServerSession,
-  type DefaultSession,
-  type NextAuthOptions,
-} from "next-auth";
-import { type Adapter } from "next-auth/adapters";
-import GithubProvider from "next-auth/providers/github";
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import { getServerSession, type DefaultSession, type NextAuthOptions } from 'next-auth'
+import { type Adapter } from 'next-auth/adapters'
+import GithubProvider from 'next-auth/providers/github'
 
-import { env } from "@/env";
-import { db } from "@/server/db";
+import { db } from '@/server/db'
+
+import { env } from '@/env'
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -16,19 +13,19 @@ import { db } from "@/server/db";
  *
  * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
  */
-declare module "next-auth" {
-  interface Session extends DefaultSession {
-    user: {
-      id: string;
-      // ...other properties
-      // role: UserRole;
-    } & DefaultSession["user"];
-  }
+declare module 'next-auth' {
+	interface Session extends DefaultSession {
+		user: {
+			id: string
+			// ...other properties
+			// role: UserRole;
+		} & DefaultSession['user']
+	}
 
-  // interface User {
-  //   // ...other properties
-  //   // role: UserRole;
-  // }
+	// interface User {
+	//   // ...other properties
+	//   // role: UserRole;
+	// }
 }
 
 /**
@@ -37,27 +34,27 @@ declare module "next-auth" {
  * @see https://next-auth.js.org/configuration/options
  */
 export const authOptions: NextAuthOptions = {
-  callbacks: {
-    session: ({ session, user }) => ({
-      ...session,
-      user: {
-        ...session.user,
-        id: user.id,
-      },
-    }),
-  },
-  adapter: PrismaAdapter(db) as Adapter,
-  providers: [
-    GithubProvider({
-      clientId: env.GITHUB_CLIENT_ID,
-      clientSecret: env.GITHUB_CLIENT_SECRET,
-    }),
-  ],
-};
+	callbacks: {
+		session: ({ session, user }) => ({
+			...session,
+			user: {
+				...session.user,
+				id: user.id
+			}
+		})
+	},
+	adapter: PrismaAdapter(db) as Adapter,
+	providers: [
+		GithubProvider({
+			clientId: env.GITHUB_CLIENT_ID,
+			clientSecret: env.GITHUB_CLIENT_SECRET
+		})
+	]
+}
 
 /**
  * Wrapper for `getServerSession` so that you don't need to import the `authOptions` in every file.
  *
  * @see https://next-auth.js.org/configuration/nextjs
  */
-export const getServerAuthSession = () => getServerSession(authOptions);
+export const getServerAuthSession = () => getServerSession(authOptions)
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,15 @@
-import { type Config } from "tailwindcss";
-import { fontFamily } from "tailwindcss/defaultTheme";
+import { fontFamily } from 'tailwindcss/defaultTheme'
+
+import { type Config } from 'tailwindcss'
 
 export default {
-  content: ["./src/**/*.tsx"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["var(--font-geist-sans)", ...fontFamily.sans],
-      },
-    },
-  },
-  plugins: [],
-} satisfies Config;
+	content: ['./src/**/*.tsx'],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['var(--font-geist-sans)', ...fontFamily.sans]
+			}
+		}
+	},
+	plugins: []
+} satisfies Config
