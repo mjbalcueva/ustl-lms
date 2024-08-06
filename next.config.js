04fdf7d0c862390665dfbd708ce4d614c71df312
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2024-08-07 07:30:34 +0800

    feat: shadcn/ui init

--- next.config.js
+++ next.config.js
@@ -5,6 +5,16 @@
 await import('./src/env.js')
 
 /** @type {import("next").NextConfig} */
-const config = {}
+const config = {
+	experimental: {
+		optimizePackageImports: [
+			'@/client/components/navigation',
+			'@/client/components/ui',
+			'@/client/context',
+			'@/client/lib/hooks',
+			'@/shared/types'
+		]
+	}
+}
 
 export default config
