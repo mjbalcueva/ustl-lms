437c9d22fae73587ab8ab72b8a6e4f160ddf6e39
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2024-08-07 07:21:36 +0800

    connect db to vercel storage

--- src/env.js
+++ src/env.js
@@ -8,6 +8,14 @@ export const env = createEnv({
 	 */
 	server: {
 		DATABASE_URL: z.string().url(),
+		POSTGRES_DATABASE: z.string(),
+		POSTGRES_HOST: z.string(),
+		POSTGRES_PASSWORD: z.string(),
+		POSTGRES_PRISMA_URL: z.string(),
+		POSTGRES_URL: z.string(),
+		POSTGRES_URL_NON_POOLING: z.string(),
+		POSTGRES_URL_NO_SSL: z.string(),
+		POSTGRES_USER: z.string(),
 		NODE_ENV: z.enum(['development', 'test', 'production']).default('development'),
 		NEXTAUTH_SECRET: process.env.NODE_ENV === 'production' ? z.string() : z.string().optional(),
 		NEXTAUTH_URL: z.preprocess(
@@ -36,6 +44,14 @@ export const env = createEnv({
 	 */
 	runtimeEnv: {
 		DATABASE_URL: process.env.DATABASE_URL,
+		POSTGRES_DATABASE: process.env.POSTGRES_DATABASE,
+		POSTGRES_HOST: process.env.POSTGRES_HOST,
+		POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
+		POSTGRES_PRISMA_URL: process.env.POSTGRES_PRISMA_URL,
+		POSTGRES_URL: process.env.POSTGRES_URL,
+		POSTGRES_URL_NON_POOLING: process.env.POSTGRES_URL_NON_POOLING,
+		POSTGRES_URL_NO_SSL: process.env.POSTGRES_URL_NO_SSL,
+		POSTGRES_USER: process.env.POSTGRES_USER,
 		NODE_ENV: process.env.NODE_ENV,
 		NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
 		NEXTAUTH_URL: process.env.NEXTAUTH_URL,
