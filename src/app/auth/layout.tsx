d775f7fda455c7d01130dcafd65c7a3a5a846614
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2024-08-08 05:44:35 +0800

    feat: simple auth background

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,7 +1,16 @@
 export default function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
 	return (
-		<div className="flex h-full items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-background to-popover">
+		<div className="flex h-full items-center justify-center bg-popover bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-background to-popover">
 			{children}
 		</div>
 	)
 }
+
+// <div className="bg-dot-white/[0.08] relative flex h-full items-center justify-center overflow-hidden bg-popover antialiased">
+// 	<div className="pointer-events-none absolute inset-0 bg-popover [mask-image:radial-gradient(ellipse_at_center,transparent_20%,white)]" />
+// 	{children}
+// </div>
+// <div className="bg-grid-white/[0.02] relative flex h-full items-center justify-center overflow-hidden bg-popover antialiased">
+// 	<div className="pointer-events-none absolute inset-0 bg-popover [mask-image:radial-gradient(ellipse_at_center,transparent_20%,white)]" />
+// 	{children}
+// </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,36 @@ import { type Config } from 'tailwindcss'
 
 /* eslint-disable */
 const { default: flattenColorPalette } = require('tailwindcss/lib/util/flattenColorPalette')
+const colors = require('tailwindcss/colors')
+const svgToDataUri = require('mini-svg-data-uri')
+
+const addBackgroundUtilities = ({ matchUtilities, theme }: any) => {
+	matchUtilities(
+		{
+			'bg-grid-lg': (value: any) => ({
+				backgroundImage: `url("${svgToDataUri(
+					`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="64" height="64" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+				)}")`
+			}),
+			'bg-grid': (value: any) => ({
+				backgroundImage: `url("${svgToDataUri(
+					`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="24" height="24" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+				)}")`
+			}),
+			'bg-grid-small': (value: any) => ({
+				backgroundImage: `url("${svgToDataUri(
+					`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+				)}")`
+			}),
+			'bg-dot': (value: any) => ({
+				backgroundImage: `url("${svgToDataUri(
+					`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
+				)}")`
+			})
+		},
+		{ values: flattenColorPalette(theme('backgroundColor')), type: 'color' }
+	)
+}
 
 const addVariablesForColors = ({ addBase, theme }: any) => {
 	const allColors = flattenColorPalette(theme('colors'))
@@ -79,7 +109,7 @@ const config = {
 			}
 		}
 	},
-	plugins: [addVariablesForColors, require('tailwindcss-animate')]
+	plugins: [addBackgroundUtilities, addVariablesForColors, require('tailwindcss-animate')]
 } satisfies Config
 
 export default config
