c018b0852407cf339953a2f7b2475171fecae3ea
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2024-08-14 12:14:49 +0800

    bug fix

--- src/client/components/navigation/top-nav.tsx
+++ src/client/components/navigation/top-nav.tsx
@@ -23,10 +23,8 @@ export const TopNav = ({
 	const { scrollYProgress } = useScroll()
 
 	useMotionValueEvent(scrollYProgress, 'change', (current) => {
-		if (typeof current !== 'number') return
-		const direction = current - scrollYProgress.getPrevious()!
-		if (current === 1) setShowNav(true)
-		else !isNavOpen && setShowNav(direction < 0)
+		const previous = scrollYProgress.getPrevious()!
+		setShowNav(previous === 0 || previous > current)
 	})
 
 	const toggleScroll = useCallback((disable: boolean) => {
@@ -38,127 +36,123 @@ export const TopNav = ({
 	}, [isNavOpen, toggleScroll])
 
 	return (
-		<>
-			<AnimatePresence>
-				<motion.aside
-					initial={{
-						opacity: 1,
-						y: 0
-					}}
-					animate={{
-						y: showNav ? 0 : -100, // Hide when scrolling down
-						opacity: showNav ? 1 : 0
-					}}
-					transition={{
-						duration: 0.2
-					}}
-					className={cn(
-						'sticky top-0 flex h-14 items-center justify-between border-b border-border bg-popover/50 px-2 backdrop-blur-xl sm:px-4 md:hidden',
-						className
-					)}
-					{...props}
+		<AnimatePresence>
+			<motion.aside
+				key="nav"
+				initial={{
+					opacity: 1,
+					y: 0
+				}}
+				animate={{
+					y: showNav ? 0 : -100, // Hide when scrolling down
+					opacity: showNav ? 1 : 0
+				}}
+				transition={{
+					duration: 0.2
+				}}
+				className={cn(
+					'sticky top-0 flex h-14 items-center justify-between border-b border-border bg-popover/50 px-2 backdrop-blur-xl sm:px-4 md:hidden',
+					className
+				)}
+				{...props}
+			>
+				<Button
+					variant={'ghost'}
+					size={'icon'}
+					onClick={() => setNavOpen(!isNavOpen)}
+					className="outline-none ring-offset-background focus-visible:ring-2 focus-visible:ring-ring"
+					aria-label={isNavOpen ? 'Close navigation menu' : 'Open navigation menu'}
 				>
-					<Button
-						variant={'ghost'}
-						size={'icon'}
-						onClick={() => setNavOpen(!isNavOpen)}
-						className="outline-none ring-offset-background focus-visible:ring-2 focus-visible:ring-ring"
-						aria-label={isNavOpen ? 'Close navigation menu' : 'Open navigation menu'}
-					>
-						{isNavOpen ? (
-							<Icons.sidebarClose className="h-6 w-6 text-popover-foreground" />
-						) : (
-							<Icons.sidebarOpen className="h-6 w-6 text-popover-foreground" />
-						)}
-					</Button>
+					{isNavOpen ? (
+						<Icons.sidebarClose className="h-6 w-6 text-popover-foreground" />
+					) : (
+						<Icons.sidebarOpen className="h-6 w-6 text-popover-foreground" />
+					)}
+				</Button>
 
-					<Link
-						className="flex items-center justify-center gap-3 rounded-md p-1 text-lg font-semibold tracking-wide outline-none ring-offset-background focus-visible:ring-2 focus-visible:ring-ring"
-						href="/dashboard"
-						aria-label="Home"
-					>
-						<Icons.logo2 />
-					</Link>
+				<Link
+					className="flex items-center justify-center gap-3 rounded-md p-1 text-lg font-semibold tracking-wide outline-none ring-offset-background focus-visible:ring-2 focus-visible:ring-ring"
+					href="/dashboard"
+					aria-label="Home"
+				>
+					<Icons.logo2 />
+				</Link>
 
-					<UserButton
-						onOpenChange={(open) => {
-							setNavOpen(false)
-							toggleScroll(open)
-						}}
-					/>
-				</motion.aside>
-			</AnimatePresence>
+				<UserButton
+					onOpenChange={(open) => {
+						setNavOpen(false)
+						toggleScroll(open)
+					}}
+				/>
+			</motion.aside>
 
-			<AnimatePresence>
-				{isNavOpen && (
-					<motion.nav
-						animate={isNavOpen ? 'animate' : 'exit'}
-						initial="initial"
-						exit="exit"
-						variants={{
-							initial: {
-								opacity: 0,
-								scale: 1
-							},
-							animate: {
-								scale: 1,
-								opacity: 1,
-								transition: {
-									duration: 0.2,
-									ease: 'easeOut'
-								}
-							},
-							exit: {
-								opacity: 0,
-								transition: {
-									duration: 0.2,
-									delay: 0.2,
-									ease: 'easeOut'
-								}
+			<motion.nav
+				key="nav-content"
+				animate={isNavOpen ? 'animate' : 'exit'}
+				initial="initial"
+				exit="exit"
+				variants={{
+					initial: {
+						opacity: 0,
+						scale: 1
+					},
+					animate: {
+						scale: 1,
+						opacity: 1,
+						transition: {
+							duration: 0.2,
+							ease: 'easeOut'
+						}
+					},
+					exit: {
+						opacity: 0,
+						transition: {
+							duration: 0.2,
+							delay: 0.2,
+							ease: 'easeOut'
+						}
+					}
+				}}
+				className={cn(
+					'fixed top-14 h-full w-full overflow-auto bg-popover/50 backdrop-blur-xl md:hidden',
+					!isNavOpen && 'pointer-events-none'
+				)}
+			>
+				<motion.ul
+					animate={isNavOpen ? 'open' : 'exit'}
+					initial="initial"
+					variants={{
+						open: {
+							transition: {
+								staggerChildren: 0.06
 							}
-						}}
-						className={cn(
-							'fixed top-14 h-full w-full overflow-auto bg-popover/50 backdrop-blur-xl md:hidden',
-							!isNavOpen && 'pointer-events-none'
-						)}
-					>
-						<motion.ul
-							animate={isNavOpen ? 'open' : 'exit'}
-							initial="initial"
+						}
+					}}
+					className="flex flex-col ease-in"
+				>
+					{navLinks.map((item, index) => (
+						<motion.li
+							key={index}
 							variants={{
+								initial: {
+									y: '-30px',
+									opacity: 0
+								},
 								open: {
+									y: 0,
+									opacity: 1,
 									transition: {
-										staggerChildren: 0.06
+										duration: 0.3,
+										ease: 'easeOut'
 									}
 								}
 							}}
-							className="flex flex-col ease-in"
 						>
-							{navLinks.map((item, index) => (
-								<motion.li
-									key={index}
-									variants={{
-										initial: {
-											y: '-30px',
-											opacity: 0
-										},
-										open: {
-											y: 0,
-											opacity: 1,
-											transition: {
-												duration: 0.3,
-												ease: 'easeOut'
-											}
-										}
-									}}
-								>
-									<NavLinkComponent link={item} className="h-12 rounded-none border-b border-border md:rounded-md" />
-								</motion.li>
-							))}
-						</motion.ul>
-					</motion.nav>
-				)}
-			</AnimatePresence>
-		</>
+							<NavLinkComponent link={item} className="h-12 rounded-none border-b border-border md:rounded-md" />
+						</motion.li>
+					))}
+				</motion.ul>
+			</motion.nav>
+		</AnimatePresence>
 	)
 }
