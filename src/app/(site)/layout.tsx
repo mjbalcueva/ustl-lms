44eaff7866378025739342f67470697080a3784e
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2024-08-18 14:21:49 +0800

    move providers inside site layout

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,19 +1,26 @@
+import { cookies } from 'next/headers'
 import { SessionProvider } from 'next-auth/react'
 
 import { auth } from '@/server/auth'
 
 import { MainNav } from '@/client/components/navigation'
+import { DeviceTypeProvider, type DeviceType } from '@/client/context'
 
 export default async function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
 	const session = await auth()
-	if (!session) return null
+	const deviceSizeCookie = cookies().get('device-size')
+
+	let defaultDeviceSize: DeviceType = ''
+	if (deviceSizeCookie) defaultDeviceSize = JSON.parse(deviceSizeCookie.value) as DeviceType
 
 	return (
 		<SessionProvider session={session}>
-			<div className="flex flex-col bg-popover/50 text-foreground md:h-[100vh] md:flex-row md:bg-popover md:pt-3">
-				<MainNav className="bg-popover/50 text-popover-foreground md:bg-popover" session={session} />
-				{children}
-			</div>
+			<DeviceTypeProvider defaultDeviceSize={defaultDeviceSize}>
+				<div className="flex flex-col bg-popover/50 text-foreground md:h-[100vh] md:flex-row md:bg-popover md:pt-3">
+					<MainNav className="bg-popover/50 text-popover-foreground md:bg-popover" session={session!} />
+					{children}
+				</div>
+			</DeviceTypeProvider>
 		</SessionProvider>
 	)
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import '@/client/styles/globals.css'
 
 import { type Metadata } from 'next'
-import { cookies } from 'next/headers'
 import { GeistSans } from 'geist/font/sans'
 
 import { siteConfig } from '@/shared/config/site'
@@ -9,7 +8,7 @@ import { TRPCReactProvider } from '@/shared/trpc/react'
 
 import { TailwindSizeIndicator } from '@/client/components/tailwind-size-indicator'
 import { Toaster } from '@/client/components/ui'
-import { DeviceTypeProvider, ThemeProvider, type DeviceType } from '@/client/context'
+import { ThemeProvider } from '@/client/context'
 
 export const metadata: Metadata = {
 	title: siteConfig.title,
@@ -18,22 +17,15 @@ export const metadata: Metadata = {
 }
 
 export default async function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
-	const deviceSizeCookie = cookies().get('device-size')
-
-	let defaultDeviceSize: DeviceType = ''
-	if (deviceSizeCookie) defaultDeviceSize = JSON.parse(deviceSizeCookie.value) as DeviceType
-
 	return (
 		<html lang="en" className={`${GeistSans.variable}`} suppressHydrationWarning>
 			<body>
 				<TRPCReactProvider>
-					<DeviceTypeProvider defaultDeviceSize={defaultDeviceSize}>
-						<ThemeProvider>
-							{children}
-							<Toaster />
-							<TailwindSizeIndicator />
-						</ThemeProvider>
-					</DeviceTypeProvider>
+					<ThemeProvider>
+						{children}
+						<Toaster />
+						<TailwindSizeIndicator />
+					</ThemeProvider>
 				</TRPCReactProvider>
 			</body>
 		</html>
