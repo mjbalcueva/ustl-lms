5e8f7dbc01b9b393409b6a8eef752d0b7ea2f3c2
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2024-08-23 11:18:46 +0800

    reorder function

--- src/server/lib/mail.ts
+++ src/server/lib/mail.ts
@@ -10,17 +10,6 @@ const resend = new Resend(env.RESEND_API_KEY)
 const domain = env.AUTH_URL
 const supportMail = 'support@ustl-lms.tech'
 
-export async function sendVerificationEmail(email: string, token: string) {
-	const confirmLink = `${domain}/auth/new-verification?token=${token}`
-
-	await resend.emails.send({
-		from: `Scholar <${supportMail}>`,
-		to: email,
-		subject: 'Confirm your email',
-		react: VerificationEmail({ email, confirmLink })
-	})
-}
-
 export async function sendPasswordResetEmail(email: string, token: string) {
 	const resetLink = `${domain}/auth/new-password?token=${token}`
 
@@ -38,5 +27,16 @@ export async function sendTwoFactorTokenEmail(email: string, token: string) {
 		to: email,
 		subject: '2FA Code',
 		html: `<p>Your 2FA Code: ${token}</p>`
+	})
+}
+
+export async function sendVerificationEmail(email: string, token: string) {
+	const confirmLink = `${domain}/auth/new-verification?token=${token}`
+
+	await resend.emails.send({
+		from: `Scholar <${supportMail}>`,
+		to: email,
+		subject: 'Confirm your email',
+		react: VerificationEmail({ email, confirmLink })
 	})
 }
