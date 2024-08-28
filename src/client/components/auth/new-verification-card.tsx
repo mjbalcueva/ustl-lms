973dab0f2f1f234460e90d1269d996bf5b049908
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2024-08-29 00:10:08 +0800

    migrate new-verication from server action to trpc

--- src/client/components/auth/new-verification-card.tsx
+++ src/client/components/auth/new-verification-card.tsx
@@ -1,45 +1,22 @@
 'use client'
 
 import { useSearchParams } from 'next/navigation'
-import { useCallback, useEffect, useState } from 'react'
+import { useEffect } from 'react'
 
-import { newVerification } from '@/server/actions/new-verification'
+import { api } from '@/shared/trpc/react'
 
 import { CardWrapper, FormResponse } from '@/client/components/auth'
 import { Loader } from '@/client/components/loader'
 
 export const NewVerificationCard = () => {
-	const [formSuccess, setFormSuccess] = useState<string | null>(null)
-	const [formError, setFormError] = useState<string | null>(null)
-
 	const searchParams = useSearchParams()
 	const token = searchParams.get('token')
 
-	const onSubmit = useCallback(async () => {
-		if (!token) {
-			setFormError('Missing token!')
-			return
-		}
-
-		await newVerification(token)
-			.then((data) => {
-				if (data?.error) {
-					setFormSuccess(null)
-					return setFormError(data?.error)
-				}
-				if (data?.success) {
-					setFormError(null)
-					return setFormSuccess(data?.success)
-				}
-			})
-			.catch(() => {
-				setFormError('Something went wrong!')
-			})
-	}, [token])
+	const { mutate, data, error, isPending } = api.user.verifyToken.useMutation()
 
 	useEffect(() => {
-		void onSubmit()
-	}, [onSubmit])
+		mutate({ token })
+	}, [token, mutate])
 
 	return (
 		<CardWrapper
@@ -49,9 +26,9 @@ export const NewVerificationCard = () => {
 			backButtonLabel="Back to login"
 		>
 			<div className="flex w-full items-center justify-center">
-				{!formSuccess && !formError && <Loader variant="dots" size="extra-large" />}
-				<FormResponse type="error" message={formError} />
-				<FormResponse type="success" message={formSuccess} />
+				{isPending && <Loader variant="dots" size="extra-large" />}
+				<FormResponse type="error" message={error?.message} />
+				<FormResponse type="success" message={data?.message} />
 			</div>
 		</CardWrapper>
 	)
--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -1,6 +1,7 @@
+import { TRPCError } from '@trpc/server'
 import { hash } from 'bcryptjs'
+import { z } from 'zod'
 
-import { getUserByEmail } from '@/shared/data/user'
 import { registerSchema } from '@/shared/validations/register'
 
 import { createTRPCRouter, publicProcedure } from '@/server/api/trpc'
@@ -11,10 +12,15 @@ export const userRouter = createTRPCRouter({
 	register: publicProcedure.input(registerSchema).mutation(async ({ ctx, input }) => {
 		const { name, email, password } = input
 
-		if (!email.endsWith('@ust-legazpi.edu.ph')) throw new Error('Please use your UST Legazpi email address!')
-		const existingUser = await getUserByEmail(email)
+		if (!email.endsWith('@ust-legazpi.edu.ph')) {
+			throw new TRPCError({ code: 'BAD_REQUEST', message: 'Please use your UST Legazpi email address.' })
+		}
 
-		if (existingUser) throw new Error('User already exists!')
+		const existingUser = await ctx.db.user.findUnique({ where: { email } })
+		if (existingUser) {
+			throw new TRPCError({ code: 'CONFLICT', message: 'User with this email already exists.' })
+		}
+
 		const hashedPassword = await hash(password, 10)
 
 		await ctx.db.user.create({
@@ -29,5 +35,41 @@ export const userRouter = createTRPCRouter({
 		await sendVerificationEmail(verificationToken.email, verificationToken.token)
 
 		return { message: 'Confirmation email sent.' }
+	}),
+
+	verifyToken: publicProcedure.input(z.object({ token: z.string().nullable() })).mutation(async ({ ctx, input }) => {
+		const { token } = input
+		if (!token) {
+			throw new TRPCError({ code: 'BAD_REQUEST', message: 'No verification token provided.' })
+		}
+
+		const existingToken = await ctx.db.verificationToken.findUnique({ where: { token } })
+		if (!existingToken) {
+			throw new TRPCError({ code: 'NOT_FOUND', message: 'Verification token not found.' })
+		}
+
+		const tokenHasExpired = new Date(existingToken.expires) < new Date()
+		if (tokenHasExpired) {
+			throw new TRPCError({ code: 'BAD_REQUEST', message: 'Verification token has expired.' })
+		}
+
+		const existingUser = await ctx.db.user.findUnique({ where: { email: existingToken.email } })
+		if (!existingUser) {
+			throw new TRPCError({ code: 'NOT_FOUND', message: 'User not found.' })
+		}
+
+		await ctx.db.user.update({
+			where: { id: existingUser.id },
+			data: {
+				emailVerified: new Date(),
+				email: existingToken.email
+			}
+		})
+
+		await ctx.db.verificationToken.delete({
+			where: { id: existingToken.id }
+		})
+
+		return { message: 'Email verified!' }
 	})
 })
