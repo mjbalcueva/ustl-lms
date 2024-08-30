988758a543925b6cdb15772df1b48855a8c5e500
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2024-08-30 20:37:28 +0800

    simplify error page

--- src/app/auth/error/page.tsx
+++ src/app/auth/error/page.tsx
@@ -1,11 +1,35 @@
-import { Suspense } from 'react'
+import Image from 'next/image'
 
-import { ErrorForm } from '@/client/components/auth/forms/error'
+import { CardWrapper } from '@/client/components/auth/card-wrapper'
 
-export default function Page() {
+type ErrorPageParam = 'Configuration' | 'AccessDenied' | 'Verification'
+
+export default function Page({ searchParams }: { searchParams: { error: ErrorPageParam } }) {
+	const errorMessages: Record<ErrorPageParam, { title: string; description: string }> = {
+		AccessDenied: {
+			title: 'Access Denied!',
+			description: 'Please use your UST Legazpi email address.'
+		},
+		Configuration: {
+			title: 'Configuration Error',
+			description: 'There was an issue with the system configuration. Please contact support.'
+		},
+		Verification: {
+			title: 'Verification Required',
+			description: 'Please verify your account to proceed.'
+		}
+	}
+
+	const { title, description } = errorMessages[searchParams.error] || {
+		title: 'Something went wrong!',
+		description: 'Please contact your system administrators for assistance.'
+	}
+
 	return (
-		<Suspense>
-			<ErrorForm />
-		</Suspense>
+		<CardWrapper title={title} description={description} backButtonHref="/auth/login" backButtonLabel="Back to login">
+			<div className="flex w-full items-center justify-center">
+				<Image src="/assets/error.svg" alt="Error" className="w-52" width={100} height={100} priority />
+			</div>
+		</CardWrapper>
 	)
 }
