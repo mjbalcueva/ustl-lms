503411e8e0bf7b7bfac6649c527147d63311b77a
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2024-08-30 22:22:46 +0800

    rewrite verify email to use authcard

--- src/app/auth/verify-email/page.tsx
+++ src/app/auth/verify-email/page.tsx
@@ -1,11 +1,32 @@
-import { Suspense } from 'react'
+import { type Metadata } from 'next'
 
+import {
+	AuthCard,
+	AuthCardContent,
+	AuthCardFooter,
+	AuthCardHeader,
+	AuthCardLink
+} from '@/client/components/auth/auth-card'
 import { VerifyEmailForm } from '@/client/components/auth/forms/verify-email'
 
-export default function Page() {
+export const metadata: Metadata = {
+	title: 'Verify Your Email | Account Activation',
+	description:
+		'Complete your account setup by verifying your email address. This quick step ensures the security of your account and enables full access to our services.'
+}
+
+export default function Page({ searchParams }: { searchParams: { token: string } }) {
 	return (
-		<Suspense>
-			<VerifyEmailForm />
-		</Suspense>
+		<AuthCard>
+			<AuthCardHeader title="Confirming your verification" description="We just want to make sure it's really you." />
+			<AuthCardContent>
+				<VerifyEmailForm token={searchParams.token} />
+			</AuthCardContent>
+			<AuthCardFooter>
+				<AuthCardLink href="/auth/login" label="Back to login" />
+				<AuthCardLink href="/privacy" label="Privacy" className="!ml-auto" />
+				<AuthCardLink href="/terms" label="Terms" />
+			</AuthCardFooter>
+		</AuthCard>
 	)
 }
--- src/client/components/auth/forms/verify-email.tsx
+++ src/client/components/auth/forms/verify-email.tsx
@@ -1,18 +1,13 @@
 'use client'
 
-import { useSearchParams } from 'next/navigation'
 import { useEffect } from 'react'
 
 import { api } from '@/shared/trpc/react'
 
-import { CardWrapper } from '@/client/components/auth/card-wrapper'
 import { FormResponse } from '@/client/components/auth/form-response'
 import { Loader } from '@/client/components/loader'
 
-export const VerifyEmailForm = () => {
-	const searchParams = useSearchParams()
-	const token = searchParams.get('token')
-
+export const VerifyEmailForm = ({ token }: { token: string }) => {
 	const { mutate, data, error, isPending } = api.auth.verifyEmail.useMutation()
 
 	useEffect(() => {
@@ -20,17 +15,10 @@ export const VerifyEmailForm = () => {
 	}, [token, mutate])
 
 	return (
-		<CardWrapper
-			title="Confirming your verification"
-			description="We just want to make sure it's really you."
-			backButtonHref="/auth/login"
-			backButtonLabel="Back to login"
-		>
-			<div className="flex w-full items-center justify-center">
-				{isPending && <Loader variant="dots" size="extra-large" />}
-				<FormResponse type="error" message={error?.message} />
-				<FormResponse type="success" message={data?.message} />
-			</div>
-		</CardWrapper>
+		<>
+			{isPending && <Loader variant="dots" size="extra-large" className="mx-auto" />}
+			<FormResponse type="error" message={error?.message} />
+			<FormResponse type="success" message={data?.message} />
+		</>
 	)
 }
