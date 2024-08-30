cae1f7d69e6bd238e3a10640de470c126a7ab162
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2024-08-30 11:32:51 +0800

    better form response variants

--- src/client/components/auth/form-response.tsx
+++ src/client/components/auth/form-response.tsx
@@ -1,34 +1,38 @@
+import * as React from 'react'
+import { cva, type VariantProps } from 'class-variance-authority'
+
 import { Icons } from '@/client/components/icons'
 import { cn } from '@/client/lib/utils'
 
-type FormResponseType = 'success' | 'error'
+const formResponseVariants = cva('flex items-center rounded-xl border p-3', {
+	variants: {
+		type: {
+			success: 'border-emerald-500/15 bg-emerald-500/15 text-emerald-500',
+			error: 'border-destructive/15 bg-destructive/15 text-destructive'
+		}
+	},
+	defaultVariants: {
+		type: 'success'
+	}
+})
 
-type FormResponseProps = React.HTMLAttributes<HTMLDivElement> & {
-	type: FormResponseType
-	message?: string | null
-}
-
-const formResponseIcon: Record<FormResponseType, React.ReactNode> = {
-	success: <Icons.circleCheckFilled className="size-4 min-h-4 min-w-4" />,
-	error: <Icons.triangleAlertFilled className="size-4 min-h-4 min-w-4" />
-}
+type FormResponseProps = React.HTMLAttributes<HTMLDivElement> &
+	VariantProps<typeof formResponseVariants> & {
+		message?: string | null
+	}
 
 export function FormResponse({ type, message, className, ...props }: FormResponseProps) {
 	if (!message) return null
 
+	const formResponseIcon: Record<string, React.ReactNode> = {
+		success: <Icons.circleCheckFilled className="size-4 min-h-4 min-w-4" />,
+		error: <Icons.triangleAlertFilled className="size-4 min-h-4 min-w-4" />
+	}
+
 	return (
-		<div
-			className={cn(
-				'flex items-center rounded-xl border p-3',
-				type === 'success'
-					? 'border-emerald-500/15 bg-emerald-500/15 text-emerald-500'
-					: 'border-destructive/15 bg-destructive/15 text-destructive',
-				className
-			)}
-			{...props}
-		>
+		<div className={cn(formResponseVariants({ type, className }))} {...props}>
 			<div className="flex items-end gap-2 text-sm leading-none">
-				{formResponseIcon[type]}
+				{formResponseIcon[type ?? 'success']}
 				{message}
 			</div>
 		</div>
