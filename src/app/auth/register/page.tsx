4f78f1f6b1be319db197e5087086f3af573b5ca3
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2024-08-30 22:33:45 +0800

    rewrite register page to use authcard

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,17 +1,34 @@
 import { type Metadata } from 'next'
-import { Suspense } from 'react'
 
+import {
+	AuthCard,
+	AuthCardContent,
+	AuthCardFooter,
+	AuthCardHeader,
+	AuthCardLink,
+	AuthCardSeparator
+} from '@/client/components/auth/auth-card'
 import { RegisterForm } from '@/client/components/auth/forms/register'
 
 export const metadata: Metadata = {
-	title: 'Register',
-	description: 'Register for an account'
+	title: 'Register an account',
+	description:
+		'Create your account to join the Thomasian community. Experience a seamless registration process and unlock exclusive features.'
 }
 
 export default function Page() {
 	return (
-		<Suspense>
-			<RegisterForm />
-		</Suspense>
+		<AuthCard>
+			<AuthCardHeader title="Welcome, Thomasian!" description="Join our community by creating your account." />
+			<AuthCardContent>
+				<RegisterForm />
+				<AuthCardSeparator separatorText="Or continue with" />
+			</AuthCardContent>
+			<AuthCardFooter>
+				<AuthCardLink href="/auth/login" label="Already have an account?" />
+				<AuthCardLink href="/privacy" label="Privacy" className="!ml-auto" />
+				<AuthCardLink href="/terms" label="Terms" />
+			</AuthCardFooter>
+		</AuthCard>
 	)
 }
--- src/client/components/auth/forms/register.tsx
+++ src/client/components/auth/forms/register.tsx
@@ -6,7 +6,6 @@ import { useForm, type SubmitHandler } from 'react-hook-form'
 import { api } from '@/shared/trpc/react'
 import { registerSchema, type RegisterSchema } from '@/shared/validations/register'
 
-import { CardWrapper } from '@/client/components/auth/card-wrapper'
 import { FormResponse } from '@/client/components/auth/form-response'
 import { Loader } from '@/client/components/loader'
 import {
@@ -35,83 +34,75 @@ export const RegisterForm = () => {
 	const onSubmit: SubmitHandler<RegisterSchema> = (data) => mutate(data)
 
 	return (
-		<CardWrapper
-			title="Welcome, Thomasian!"
-			description="To get started, please create an account."
-			backButtonLabel="Already have an account?"
-			backButtonHref="/auth/login"
-			showSocial
-		>
-			<Form {...form}>
-				<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-					<FormField
-						control={form.control}
-						name="name"
-						render={({ field }) => (
-							<FormItem>
-								<FormLabel className="text-card-foreground">Name</FormLabel>
-								<FormControl>
-									<Input
-										placeholder="What should we call you?"
-										className="rounded-xl bg-background"
-										autoComplete="name"
-										{...field}
-									/>
-								</FormControl>
-								<FormMessage />
-							</FormItem>
+		<Form {...form}>
+			<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+				<FormField
+					control={form.control}
+					name="name"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel className="text-card-foreground">Name</FormLabel>
+							<FormControl>
+								<Input
+									placeholder="What should we call you?"
+									className="rounded-xl bg-background"
+									autoComplete="name"
+									{...field}
+								/>
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+
+				<FormField
+					control={form.control}
+					name="email"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel className="text-card-foreground">Email Address</FormLabel>
+							<FormControl>
+								<Input
+									placeholder="Enter your email"
+									className="rounded-xl bg-background"
+									type="email"
+									autoComplete="email"
+									{...field}
+								/>
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+
+				<FormField
+					control={form.control}
+					name="password"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel className="text-card-foreground">Password</FormLabel>
+							<FormControl>
+								<PasswordInput placeholder="Password" className="rounded-xl bg-background" {...field} />
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+
+				<FormResponse type="error" message={error?.message} />
+				<FormResponse type="success" message={data?.message} />
+
+				<div className="pt-2">
+					<ButtonShining className="w-full rounded-xl" shiningClassName="bg-white/20" disabled={isPending}>
+						{isPending && (
+							<span className="relative right-[4.5ch]">
+								<Loader />
+							</span>
 						)}
-					/>
-
-					<FormField
-						control={form.control}
-						name="email"
-						render={({ field }) => (
-							<FormItem>
-								<FormLabel className="text-card-foreground">Email Address</FormLabel>
-								<FormControl>
-									<Input
-										placeholder="Enter your email"
-										className="rounded-xl bg-background"
-										type="email"
-										autoComplete="email"
-										{...field}
-									/>
-								</FormControl>
-								<FormMessage />
-							</FormItem>
-						)}
-					/>
-
-					<FormField
-						control={form.control}
-						name="password"
-						render={({ field }) => (
-							<FormItem>
-								<FormLabel className="text-card-foreground">Password</FormLabel>
-								<FormControl>
-									<PasswordInput placeholder="Password" className="rounded-xl bg-background" {...field} />
-								</FormControl>
-								<FormMessage />
-							</FormItem>
-						)}
-					/>
-
-					<FormResponse type="error" message={error?.message} />
-					<FormResponse type="success" message={data?.message} />
-
-					<div className="pt-2">
-						<ButtonShining className="w-full rounded-xl" shiningClassName="bg-white/20" disabled={isPending}>
-							{isPending && (
-								<span className="relative right-[4.5ch]">
-									<Loader />
-								</span>
-							)}
-							<span className="absolute">Register</span>
-						</ButtonShining>
-					</div>
-				</form>
-			</Form>
-		</CardWrapper>
+						<span className="absolute">Register</span>
+					</ButtonShining>
+				</div>
+			</form>
+		</Form>
 	)
 }
