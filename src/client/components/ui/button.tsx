c6acd20dbf36c12b251b6379d3a650576dc8a179
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2024-08-30 21:23:40 +0800

    add new size in buttonvariants

--- src/client/components/ui/button.tsx
+++ src/client/components/ui/button.tsx
@@ -18,10 +18,11 @@ const buttonVariants = cva(
 			},
 			size: {
 				default: 'h-10 px-4 py-2',
+				icon: 'h-10 w-10',
 				xs: 'px-8 h-7 focus-visible:ring-offset-2',
 				sm: 'h-9 rounded-md px-3',
 				lg: 'h-11 rounded-md px-8',
-				icon: 'h-10 w-10'
+				link: 'h-7 text-xs focus-visible:ring-offset-2'
 			}
 		},
 		defaultVariants: {
