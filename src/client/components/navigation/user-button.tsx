471970c63cc95a04eed940c78aad108d351fc44b
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2024-09-01 05:45:56 +0800

    make user button avatar unselectable

--- src/client/components/navigation/user-button.tsx
+++ src/client/components/navigation/user-button.tsx
@@ -48,7 +48,7 @@ export const UserButton: React.FC<UserButtonProps> = ({ session, ...props }: Use
 		<DropdownMenu modal={false} {...props}>
 			<DropdownMenuTrigger className="flex cursor-pointer items-center gap-3 rounded-md p-1 outline-none hover:bg-accent md:min-h-[2.8rem]">
 				<Avatar className="size-8 border border-border md:ml-[1.5px]">
-					<AvatarImage src={user?.image ?? ''} alt={initials} />
+					<AvatarImage src={user?.image ?? ''} alt={initials} className="select-none" />
 					<AvatarFallback className="pointer-events-none select-none bg-muted">{initials}</AvatarFallback>
 				</Avatar>
 				{!isMobile && (
