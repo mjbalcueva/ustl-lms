8912d2ca3f53f07433e0839f9c0eb48bd7cdb13d
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2024-09-01 14:12:01 +0800

    add role

--- src/shared/types/navigation.ts
+++ src/shared/types/navigation.ts
@@ -1,7 +1,10 @@
+import { type UserRole } from '@prisma/client'
+
 import { type Icons } from '@/client/components/icons'
 
 export interface Link {
 	label: string
 	href: string
 	icon: keyof typeof Icons
+	role?: UserRole[]
 }
