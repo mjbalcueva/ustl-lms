5cdec1bacc27c6be82fa80e802e658925ee9a446
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2024-09-08 14:23:13 +0800

    fix breadcrumbs look

--- src/app/(site)/(instructor)/course/page.tsx
+++ src/app/(site)/(instructor)/course/page.tsx
@@ -2,19 +2,17 @@ import Link from 'next/link'
 
 import { Icons } from '@/client/components/icons'
 import { PageBreadcrumbs } from '@/client/components/page-breadcrumbs'
-import { PageContent, PageDescription, PageHeader, PageTitle, PageWrapper } from '@/client/components/page-wrapper'
+import { PageContent, PageHeader, PageTitle, PageWrapper } from '@/client/components/page-wrapper'
 import { buttonVariants, Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/client/components/ui'
 import { cn } from '@/client/lib/utils'
 
 export default function Page() {
 	return (
 		<PageWrapper>
-			<PageBreadcrumbs withIcons />
-
 			<PageHeader className="flex items-end justify-between space-y-0">
 				<div className="space-y-1.5">
-					<PageTitle className="font-bold">Manage Your Courses</PageTitle>
-					<PageDescription>Create, manage, and publish your courses!</PageDescription>
+					<PageTitle className="font-bold">Courses</PageTitle>
+					<PageBreadcrumbs withIcons />
 				</div>
 				<Link href="/course/create" className={cn(buttonVariants(), 'h-10 w-10 sm:w-auto')}>
 					<Icons.plusCircle className="size-5 shrink-0 sm:mr-2" />
