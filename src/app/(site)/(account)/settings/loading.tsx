9d2aef1bf10792e29db942f897940b8583b9e840
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2024-09-08 19:04:41 +0800

    rewrite app to add pageSection

--- src/app/(site)/(account)/settings/loading.tsx
+++ src/app/(site)/(account)/settings/loading.tsx
@@ -4,6 +4,7 @@ import {
 	PageContent,
 	PageDescription,
 	PageHeader,
+	PageSection,
 	PageTitle,
 	PageWrapper
 } from '@/client/components/page-wrapper'
@@ -17,47 +18,52 @@ export default function Loading() {
 					<PageTitle>Account Settings</PageTitle>
 					<PageDescription>Manage your account security settings</PageDescription>
 				</PageHeader>
-				<PageContent className="space-y-4">
-					<ItemWrapper>
-						<ItemHeader>
-							<Skeleton className="h-6 w-40" />
-							<Skeleton className="h-4" />
-						</ItemHeader>
-						<ItemContent withSeparator>
-							<Skeleton className="h-20" />
-						</ItemContent>
-						<ItemFooter className="space-x-2">
-							<Skeleton className="h-4 w-4" />
-							<Skeleton className="h-4 w-20" />
-							<Skeleton className="!ml-auto h-8 w-16" />
-						</ItemFooter>
-					</ItemWrapper>
 
-					<ItemWrapper>
-						<ItemHeader>
-							<Skeleton className="h-6 w-40" />
-							<Skeleton className="h-4" />
-						</ItemHeader>
-						<ItemContent className="space-y-4" withSeparator>
-							<div className="space-y-2">
-								<Skeleton className="h-4 w-40" />
-								<Skeleton className="h-10 md:w-3/4" />
-							</div>
-							<div className="space-y-2">
-								<Skeleton className="h-4 w-32" />
-								<Skeleton className="h-10 md:w-3/4" />
-							</div>
-							<div className="space-y-2">
-								<Skeleton className="h-4 w-48" />
-								<Skeleton className="h-10 md:w-3/4" />
-							</div>
-						</ItemContent>
-						<ItemFooter className="space-x-2">
-							<Skeleton className="h-4 w-4" />
-							<Skeleton className="h-4 w-20" />
-							<Skeleton className="!ml-auto h-8 w-16" />
-						</ItemFooter>
-					</ItemWrapper>
+				<PageContent className="space-y-4">
+					<PageSection>
+						<ItemWrapper>
+							<ItemHeader>
+								<Skeleton className="h-6 w-40" />
+								<Skeleton className="h-4" />
+							</ItemHeader>
+							<ItemContent withSeparator>
+								<Skeleton className="h-20" />
+							</ItemContent>
+							<ItemFooter className="space-x-2">
+								<Skeleton className="h-4 w-4" />
+								<Skeleton className="h-4 w-20" />
+								<Skeleton className="!ml-auto h-8 w-16" />
+							</ItemFooter>
+						</ItemWrapper>
+					</PageSection>
+
+					<PageSection>
+						<ItemWrapper>
+							<ItemHeader>
+								<Skeleton className="h-6 w-40" />
+								<Skeleton className="h-4" />
+							</ItemHeader>
+							<ItemContent className="space-y-4" withSeparator>
+								<div className="space-y-2">
+									<Skeleton className="h-4 w-40" />
+									<Skeleton className="h-10 md:w-3/4" />
+								</div>
+								<div className="space-y-2">
+									<Skeleton className="h-4 w-32" />
+									<Skeleton className="h-10 md:w-3/4" />
+								</div>
+								<div className="space-y-2">
+									<Skeleton className="h-4 w-48" />
+									<Skeleton className="h-10 md:w-3/4" />
+								</div>
+							</ItemContent>
+							<ItemFooter className="space-x-2">
+								<Skeleton className="h-4 w-4" />
+								<Skeleton className="h-4 w-20" />
+								<Skeleton className="!ml-auto h-8 w-16" />
+							</ItemFooter>
+						</ItemWrapper>
+					</PageSection>
 				</PageContent>
 			</PageContainer>
 		</PageWrapper>
