45a38f028cf00d51de5b1f0688ad6341633ffa3d
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2024-09-12 14:35:03 +0800

    code is required now

--- src/client/components/instructor/course/forms/create-course.tsx
+++ src/client/components/instructor/course/forms/create-course.tsx
@@ -8,26 +8,18 @@ import { toast } from 'sonner'
 import { api } from '@/shared/trpc/react'
 import { createCourseSchema, type CreateCourseSchema } from '@/shared/validations/course'
 
+import { Icons } from '@/client/components/icons'
 import {
 	ItemContent,
 	ItemDescription,
 	ItemFooter,
+	ItemFooterDescription,
 	ItemHeader,
 	ItemTitle,
 	ItemWrapper
 } from '@/client/components/item-wrapper'
 import { Loader } from '@/client/components/loader'
-import {
-	Button,
-	Form,
-	FormControl,
-	FormDescription,
-	FormField,
-	FormItem,
-	FormLabel,
-	FormMessage,
-	Input
-} from '@/client/components/ui'
+import { Button, Form, FormControl, FormField, FormItem, FormLabel, FormMessage, Input } from '@/client/components/ui'
 
 export const CreateCourseForm = () => {
 	const router = useRouter()
@@ -35,6 +27,7 @@ export const CreateCourseForm = () => {
 	const form = useForm<CreateCourseSchema>({
 		resolver: zodResolver(createCourseSchema),
 		defaultValues: {
+			code: '',
 			title: ''
 		}
 	})
@@ -56,38 +49,57 @@ export const CreateCourseForm = () => {
 		<ItemWrapper className="w-full">
 			<ItemHeader>
 				<ItemTitle className="text-2xl font-semibold">Name your course</ItemTitle>
-				<ItemDescription>
-					What do you want to call your course? Don&apos;t worry, you can change this later.
-				</ItemDescription>
+				<ItemDescription>What do you want to call your course?</ItemDescription>
 			</ItemHeader>
 
 			<Form {...form}>
 				<form onSubmit={form.handleSubmit(onSubmit)}>
-					<ItemContent>
-						<FormField
-							control={form.control}
-							name="title"
-							render={({ field }) => (
-								<FormItem>
-									<FormLabel className="text-card-foreground">Course Title</FormLabel>
-									<FormControl>
-										<Input className="rounded-xl sm:w-3/5" placeholder="Enter a new name" {...field} />
-									</FormControl>
-									<FormDescription>What will you teach in this course?</FormDescription>
-									<FormMessage />
-								</FormItem>
-							)}
-						/>
+					<ItemContent withSeparator>
+						<div className="grid grid-cols-3 gap-4">
+							<FormField
+								control={form.control}
+								name="code"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel className="text-card-foreground">Course Code</FormLabel>
+										<FormControl>
+											<Input className="rounded-xl" placeholder="Enter a course code" {...field} />
+										</FormControl>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+
+							<FormField
+								control={form.control}
+								name="title"
+								render={({ field }) => (
+									<FormItem className="col-span-2">
+										<FormLabel className="text-card-foreground">Course Title</FormLabel>
+										<FormControl>
+											<Input className="rounded-xl" placeholder="Enter a new name" {...field} />
+										</FormControl>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+						</div>
 					</ItemContent>
 
-					<ItemFooter className="justify-end gap-3">
-						<Button variant="outline" className="flex h-8 gap-1 text-sm">
-							Cancel
-						</Button>
-						<Button className="flex h-8 gap-1 text-sm">
-							{isPending && <Loader />}
-							{isPending ? 'Saving...' : 'Save'}
-						</Button>
+					<ItemFooter className="justify-between">
+						<div className="flex items-center gap-1">
+							<Icons.info className="h-4 w-4 text-muted-foreground" />
+							<ItemFooterDescription>Don&apos;t worry, you can still change this later.</ItemFooterDescription>
+						</div>
+						<div className="flex gap-3">
+							<Button variant="outline" className="flex h-8 gap-1 text-sm" type="button" onClick={() => router.back()}>
+								Cancel
+							</Button>
+							<Button className="flex h-8 gap-1 text-sm" type="submit" disabled={isPending}>
+								{isPending && <Loader />}
+								{isPending ? 'Saving...' : 'Save'}
+							</Button>
+						</div>
 					</ItemFooter>
 				</form>
 			</Form>
