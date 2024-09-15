ff4c7db5a44dadb83ed6e334b823bc5d2994c9a9
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2024-09-16 03:17:37 +0800

    fix, use barell

--- src/client/components/ui/password-input.tsx
+++ src/client/components/ui/password-input.tsx
@@ -3,8 +3,7 @@
 import * as React from 'react'
 import { LuEye, LuEyeOff } from 'react-icons/lu'
 
-import { Button } from '@/client/components/ui/button'
-import { Input } from '@/client/components/ui/input'
+import { Button, Input } from '@/client/components/ui'
 import { cn } from '@/client/lib/utils'
 
 type PasswordInputProps = React.ComponentProps<typeof Input> & {
