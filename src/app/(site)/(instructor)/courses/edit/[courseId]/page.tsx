f01460713b22e3471dafcae46a1307bb2187fdf3
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2024-09-24 04:04:38 +0800

    remove unused hydrate client

--- src/app/(site)/(instructor)/courses/edit/[courseId]/page.tsx
+++ src/app/(site)/(instructor)/courses/edit/[courseId]/page.tsx
@@ -1,6 +1,6 @@
 import { TbBook2, TbListDetails, TbPackage } from 'react-icons/tb'
 
-import { api, HydrateClient } from '@/shared/trpc/server'
+import { api } from '@/shared/trpc/server'
 import { type Breadcrumb } from '@/shared/types/breadcrumbs'
 
 import { AddAttachmentsForm } from '@/client/components/course/forms/add-attachments'
@@ -51,46 +51,44 @@ export default async function Page({ params }: { params: { courseId: string } })
 	]
 
 	return (
-		<HydrateClient>
-			<PageWrapper>
-				<PageHeader className="hidden space-y-0 md:block md:py-3">
-					<PageBreadcrumbs crumbs={crumbs} />
-				</PageHeader>
+		<PageWrapper>
+			<PageHeader className="hidden space-y-0 md:block md:py-3">
+				<PageBreadcrumbs crumbs={crumbs} />
+			</PageHeader>
 
-				<Separator className="hidden md:block" />
+			<Separator className="hidden md:block" />
 
-				<PageHeader>
-					<PageTitle className="font-bold">Course Setup</PageTitle>
-					<PageDescription>Completed {completionText}</PageDescription>
-				</PageHeader>
+			<PageHeader>
+				<PageTitle className="font-bold">Course Setup</PageTitle>
+				<PageDescription>Completed {completionText}</PageDescription>
+			</PageHeader>
 
-				<PageContent className="gap-4 px-2.5 sm:px-4 md:flex md:flex-wrap md:gap-6 md:px-6">
-					<PageSection className="mb-6 flex-1 md:mb-0" compactMode>
-						<PageSectionTitle title="Customize your course" icon={TbBook2} />
-						<EditCodeForm courseId={course.id} initialData={{ code: course.code }} />
-						<EditTitleForm courseId={course.id} initialData={{ title: course.title }} />
-						<EditDescriptionForm courseId={course.id} initialData={{ description: course.description ?? '' }} />
-						<EditImageForm courseId={course.id} initialData={{ image: course.image ?? '' }} />
-						<EditCategoriesForm
-							courseId={course.id}
-							categoryId={course.categoryId ?? ''}
-							options={categories.map((category) => ({ value: category.id, label: category.name }))}
-						/>
+			<PageContent className="gap-4 px-2.5 sm:px-4 md:flex md:flex-wrap md:gap-6 md:px-6">
+				<PageSection className="mb-6 flex-1 md:mb-0" compactMode>
+					<PageSectionTitle title="Customize your course" icon={TbBook2} />
+					<EditCodeForm courseId={course.id} initialData={{ code: course.code }} />
+					<EditTitleForm courseId={course.id} initialData={{ title: course.title }} />
+					<EditDescriptionForm courseId={course.id} initialData={{ description: course.description ?? '' }} />
+					<EditImageForm courseId={course.id} initialData={{ image: course.image ?? '' }} />
+					<EditCategoriesForm
+						courseId={course.id}
+						categoryId={course.categoryId ?? ''}
+						options={categories.map((category) => ({ value: category.id, label: category.name }))}
+					/>
+				</PageSection>
+
+				<div className="flex flex-1 flex-col gap-4 md:gap-6">
+					<PageSection compactMode>
+						<PageSectionTitle title="Course chapters" icon={TbListDetails} />
+						<AddChaptersForm courseId={course.id} initialData={{ chapters: course.chapter }} />
 					</PageSection>
 
-					<div className="flex flex-1 flex-col gap-4 md:gap-6">
-						<PageSection compactMode>
-							<PageSectionTitle title="Course chapters" icon={TbListDetails} />
-							<AddChaptersForm courseId={course.id} initialData={{ chapters: course.chapter }} />
-						</PageSection>
-
-						<PageSection compactMode>
-							<PageSectionTitle title="Resources & Attachments" icon={TbPackage} />
-							<AddAttachmentsForm courseId={course.id} initialData={{ attachment: course.attachment }} />
-						</PageSection>
-					</div>
-				</PageContent>
-			</PageWrapper>
-		</HydrateClient>
+					<PageSection compactMode>
+						<PageSectionTitle title="Resources & Attachments" icon={TbPackage} />
+						<AddAttachmentsForm courseId={course.id} initialData={{ attachment: course.attachment }} />
+					</PageSection>
+				</div>
+			</PageContent>
+		</PageWrapper>
 	)
 }
