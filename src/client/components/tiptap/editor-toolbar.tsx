63073f168f7a6d78d7d0ed9bb5a4dd95aa979550
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2024-09-26 23:23:08 +0800

    can edit text foreground

--- src/client/components/tiptap/editor-toolbar.tsx
+++ src/client/components/tiptap/editor-toolbar.tsx
@@ -1,5 +1,6 @@
 import { type Editor } from '@tiptap/react'
 
+import { UpdateTextForeground } from '@/client/components/tiptap/update-text-foreground'
 import { UpdateTextFormating } from '@/client/components/tiptap/update-text-formating'
 import { UpdateTextStyle } from '@/client/components/tiptap/update-text-style'
 import { Separator } from '@/client/components/ui'
@@ -10,6 +11,8 @@ export const EditorToolbar = ({ editor }: { editor: Editor }) => (
 			<UpdateTextStyle editor={editor} activeLevels={[1, 2, 3, 4, 5, 6]} />
 			<Separator orientation="vertical" className="h-7" />
 			<UpdateTextFormating editor={editor} />
+			<Separator orientation="vertical" className="h-7" />
+			<UpdateTextForeground editor={editor} />
 		</div>
 	</div>
 )
