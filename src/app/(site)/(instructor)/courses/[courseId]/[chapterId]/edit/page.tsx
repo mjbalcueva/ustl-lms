59c29304795bbd831f1240392ef8e52f9f984fdf
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2024-09-28 13:58:19 +0800

    make page title bold

--- src/app/(site)/(instructor)/courses/[courseId]/[chapterId]/edit/page.tsx
+++ src/app/(site)/(instructor)/courses/[courseId]/[chapterId]/edit/page.tsx
@@ -47,7 +47,7 @@ export default async function Page({ params }: { params: { courseId: string; cha
 			<Separator className="hidden md:block" />
 
 			<PageHeader>
-				<PageTitle>Topic Setup</PageTitle>
+				<PageTitle className="font-bold">Topic Setup</PageTitle>
 				<PageDescription>Completed {completionText}</PageDescription>
 			</PageHeader>
 
