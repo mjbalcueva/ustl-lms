4427968edebd15c8c6871b7f544d510f9a1519b1
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2024-10-03 16:22:16 +0800

    fill table

--- src/client/components/course/manage-column.tsx
+++ src/client/components/course/manage-column.tsx
@@ -1,27 +1,88 @@
 'use client'
 
+import Link from 'next/link'
+import { type Course } from '@prisma/client'
 import { type ColumnDef } from '@tanstack/react-table'
+import { LuArrowUpDown, LuMoreHorizontal, LuPencil } from 'react-icons/lu'
 
-// This type is used to define the shape of our data.
-// You can use a Zod schema here if you want.
-export type Payment = {
-	id: string
-	amount: number
-	status: 'pending' | 'processing' | 'success' | 'failed'
-	email: string
-}
+import {
+	Badge,
+	Button,
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuTrigger
+} from '@/client/components/ui'
+import { cn } from '@/client/lib/utils'
 
-export const columns: ColumnDef<Payment>[] = [
+export const columns: ColumnDef<Course>[] = [
 	{
-		accessorKey: 'status',
-		header: 'Status'
+		accessorKey: 'title',
+		header: ({ column }) => {
+			return (
+				<Button variant="ghost" size="sm" onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}>
+					Title
+					<LuArrowUpDown className="ml-2 size-3" />
+				</Button>
+			)
+		}
 	},
 	{
-		accessorKey: 'email',
-		header: 'Email'
+		accessorKey: 'description',
+		header: ({ column }) => {
+			return (
+				<Button variant="ghost" size="sm" onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}>
+					Description
+					<LuArrowUpDown className="ml-2 size-3" />
+				</Button>
+			)
+		}
 	},
 	{
-		accessorKey: 'amount',
-		header: 'Amount'
+		accessorKey: 'isPublished',
+		header: ({ column }) => {
+			return (
+				<Button variant="ghost" size="sm" onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}>
+					Published
+					<LuArrowUpDown className="ml-2 size-3" />
+				</Button>
+			)
+		},
+		cell: ({ row }) => {
+			const { isPublished } = row.original
+
+			return (
+				<Badge
+					className={cn(isPublished ? 'bg-primary text-primary-foreground' : 'bg-secondary text-secondary-foreground')}
+				>
+					{isPublished ? 'Published' : 'Draft'}
+				</Badge>
+			)
+		}
+	},
+	{
+		id: 'actions',
+		cell: ({ row }) => {
+			const { id } = row.original
+
+			return (
+				<DropdownMenu>
+					<DropdownMenuTrigger asChild>
+						<Button variant="ghost" size="icon" className="size-8 p-0">
+							<span className="sr-only">Open menu</span>
+							<LuMoreHorizontal className="size-4" />
+						</Button>
+					</DropdownMenuTrigger>
+					<DropdownMenuContent align="end">
+						<Link href={`/courses/${id}/edit`}>
+							<DropdownMenuItem>
+								<LuPencil className="mr-2 size-3.5" />
+								Edit
+							</DropdownMenuItem>
+						</Link>
+					</DropdownMenuContent>
+				</DropdownMenu>
+			)
+		}
 	}
 ]
--- src/server/api/routers/instructor.ts
+++ src/server/api/routers/instructor.ts
@@ -1,6 +1,14 @@
 import { createTRPCRouter, instructorProcedure } from '@/server/api/trpc'
 
 export const instructorRouter = createTRPCRouter({
+	getCourses: instructorProcedure.query(async ({ ctx }) => {
+		const courses = await ctx.db.course.findMany({
+			where: { createdById: ctx.session.user.id! }
+		})
+
+		return courses
+	}),
+
 	getCourseStats: instructorProcedure.query(async ({ ctx }) => {
 		const courses = await ctx.db.course.findMany({
 			where: { createdById: ctx.session.user.id! }
