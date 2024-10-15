4bfeceaee19719c9f2ae150e9034c75d50c26f75
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2024-10-15 22:03:33 +0800

    revert to old design

--- src/app/(site)/(instructor)/courses/[courseId]/[chapterId]/edit/page.tsx
+++ src/app/(site)/(instructor)/courses/[courseId]/[chapterId]/edit/page.tsx
@@ -3,14 +3,19 @@ import { redirect } from 'next/navigation'
 import { api } from '@/shared/trpc/server'
 import { type Breadcrumb } from '@/shared/types/breadcrumbs'
 
-import { ChapterTabs } from '@/client/components/course/chapter-tabs'
+import { CollapsibleSection } from '@/client/components/collapsible-section'
+import { AddChapterAttachmentsForm } from '@/client/components/course/forms/add-chapter-attachments'
 import { ChapterActions } from '@/client/components/course/forms/chapter-actions'
+import { EditChapterContentForm } from '@/client/components/course/forms/edit-chapter-content'
+import { EditChapterTitleForm } from '@/client/components/course/forms/edit-chapter-title'
+import { EditChapterVideoForm } from '@/client/components/course/forms/edit-chapter-video'
 import { type Icons } from '@/client/components/icons'
 import { NotFound } from '@/client/components/not-found'
 import {
 	Badge,
 	Banner,
 	PageBreadcrumbs,
+	PageContent,
 	PageDescription,
 	PageHeader,
 	PageTitle,
@@ -75,8 +80,28 @@ export default async function Page({ params }: { params: { courseId: string; cha
 				</div>
 				<ChapterActions id={chapter.id} courseId={chapter.course.id} status={chapter.status} type={chapter.type} />
 			</PageHeader>
+			<PageContent className="gap-4 px-2.5 sm:px-4 md:flex md:flex-wrap md:gap-6 md:px-6">
+				<div className="mb-6 flex-1 md:mb-0">
+					<CollapsibleSection title={`Customize your ${chapter.type.toLowerCase()}`} iconName="Tb/TbNotes">
+						<EditChapterTitleForm id={chapter.id} courseId={chapter.course.id} title={chapter.title} />
+						<EditChapterContentForm id={chapter.id} courseId={chapter.course.id} content={chapter.content} />
+					</CollapsibleSection>
 
-			<ChapterTabs chapter={chapter} />
+					<CollapsibleSection title="Lecture video" iconName="Tb/TbVideo">
+						<EditChapterVideoForm id={chapter.id} courseId={chapter.course.id} initialData={chapter} />
+					</CollapsibleSection>
+
+					<CollapsibleSection title="Learning materials" iconName="Tb/TbPaperclip">
+						<AddChapterAttachmentsForm
+							courseId={chapter.course.id}
+							chapterId={chapter.id}
+							attachments={chapter.attachments}
+						/>
+					</CollapsibleSection>
+				</div>
+
+				<div className="flex flex-1 flex-col gap-4 md:gap-6"></div>
+			</PageContent>
 		</PageWrapper>
 	)
 }
