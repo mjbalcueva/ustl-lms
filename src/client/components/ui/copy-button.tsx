24a6ee7687dcefcffe0d1d6ff09dd0f8b556dc45
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2024-10-19 05:25:35 +0800

    fix icon

--- src/client/components/ui/copy-button.tsx
+++ src/client/components/ui/copy-button.tsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
-import { TbCheck, TbCopy } from 'react-icons/tb'
+import { type IconType } from 'react-icons'
+import { TbCheck } from 'react-icons/tb'
 
 import { Button } from '@/client/components/ui/button'
 
 type CopyButtonProps = {
 	onCopy: () => void
+	icon: IconType
 }
 
-export const CopyButton = ({ onCopy }: CopyButtonProps) => {
+export const CopyButton = ({ onCopy, icon }: CopyButtonProps) => {
 	const [isCopied, setIsCopied] = useState(false)
 
 	const handleClick = () => {
@@ -16,9 +18,11 @@ export const CopyButton = ({ onCopy }: CopyButtonProps) => {
 		setTimeout(() => setIsCopied(false), 2000) // Revert back after 2 seconds
 	}
 
+	const Icon = icon
+
 	return (
 		<Button type="button" size="icon" variant="outline" className="px-4" onClick={handleClick}>
-			{isCopied ? <TbCheck className="size-4 shrink-0" /> : <TbCopy className="size-4 shrink-0" />}
+			{isCopied ? <TbCheck className="size-4 shrink-0" /> : <Icon className="size-4 shrink-0" />}
 		</Button>
 	)
 }
